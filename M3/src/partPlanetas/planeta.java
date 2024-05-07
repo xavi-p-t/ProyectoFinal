7424ada83874eae96a23bdc64234f7378592bdb6
xavier <xavi.palma06@gmail.com>
2024-05-07 18:58:43 +0200

    Update planeta.java

--- M3/src/partPlanetas/planeta.java
+++ M3/src/partPlanetas/planeta.java
@@ -1,5 +1,15 @@
 package partPlanetas;
 
-public class planeta {
+import java.util.ArrayList;
 
+public class planeta {
+	private int technologyDefense;
+	private int technologyAtack;
+	private int metal;
+	private int deuterium;
+	private int upgradeDefenseTechnologyDeuteriumCost;
+	private int upgradeAttackTechnologyDeuteriumCost;
+	private ArrayList<MilitaryUnit>[] army = new ArrayList[7];
+	
+	
 }
