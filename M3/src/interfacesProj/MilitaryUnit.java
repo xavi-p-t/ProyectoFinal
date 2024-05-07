8dea13ff9d485b49aefc0a6125648f5463f8b4f6
xavier <xavi.palma06@gmail.com>
2024-05-07 19:08:43 +0200

    Create MilitaryUnit.java

---
+++ M3/src/interfacesProj/MilitaryUnit.java
@@ -0,0 +1,12 @@
+package interfacesProj;
+
+public interface MilitaryUnit {
+	abstract int attack();
+	abstract void tekeDamage(int receivedDamage);
+	abstract int getActualArmor();
+	abstract int getMetalCost();
+	abstract int getDeuteriumCost();
+	abstract int getChanceGeneratinWaste();
+	abstract int getChanceAttackAgain();
+	abstract void resetArmor();
+}
