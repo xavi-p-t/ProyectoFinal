0971379c5ac455c5f85b3528bdbf4f58b58343ce
Zonoik09 <jonaneiro09@gmail.com>
2024-05-22 18:48:09 +0200

    TUTORIAL PART
    Mejora de la parte del tutorial

---
+++ M4/scripts/scroll.js
@@ -0,0 +1,15 @@
+function scrollToObjective() {
+    document.getElementById('obj').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTuto() {
+    document.getElementById('tuto').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTips() {
+    document.getElementById('tip').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollTofq() {
+    document.getElementById('fq').scrollIntoView({ behavior: 'smooth' });
+}
