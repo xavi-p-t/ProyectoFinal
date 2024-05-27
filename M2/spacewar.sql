668065686fe5efaecd7a713d84998a6217597332
xavier <104200592+xavi-p-t@users.noreply.github.com>
2024-05-27 09:19:51 +0200

    Update spacewar.sql

--- M2/spacewar.sql
+++ M2/spacewar.sql
@@ -1,9 +1,10 @@
--- Crear la tabla de usuario
+--USERS
 CREATE TABLE Users (
     user_id NUMBER PRIMARY KEY NOT NULL,
     username VARCHAR2(30) NOT NULL,
     user_password VARCHAR2(200)
 );
+
 -- Crear la tabla Planet_stats
 CREATE TABLE Planet_stats (
     planet_id NUMBER PRIMARY KEY NOT NULL,
@@ -24,70 +25,12 @@ CREATE TABLE Planet_stats (
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
--- Crear la tabla Battle_stats
-CREATE TABLE Battle_stats (
-    planet_id NUMBER NOT NULL,
-    num_battle NUMBER NOT NULL,
-    PRIMARY KEY (planet_id, num_battle), 
-    FOREIGN KEY (planet_id) REFERENCES Planet_stats(planet_id),
-    resource_metal_acquired NUMBER,
-    resource_deuterior_acquired NUMBER 
-);
-
--- Crear la tabla Battle_Log
-CREATE TABLE Battle_log (
-    planet_id NUMBER NOT NULL,
-    num_battle NUMBER NOT NULL,
-    PRIMARY KEY (planet_id, num_battle), 
-    FOREIGN KEY (planet_id) REFERENCES Planet_stats(planet_id),
-    num_line PRIMARY KEY NOT NULL,
-    log_entry VARCHAR2(100)
-);
-
--- Crear la tabla Planet_battle_defenses
-CREATE TABLE Planet_battle_defenses (
-    planet_id NUMBER NOT NULL,
-    num_battle NUMBER NOT NULL,
-    PRIMARY KEY (planet_id, num_battle), 
-    FOREIGN KEY (planet_id) REFERENCES Planet_stats(planet_id),
-    missile_launcher_built NUMBER,
-    missile_launcher_destroyed NUMBER,
-    ion_cannon_built NUMBER,
-    ion_cannon_destroyed NUMBER,
-    plasma_cannon_built NUMBER,
-    plasma_cannon_destroyed NUMBER
-);
-
--- Crear Planet_battle_army
-CREATE TABLE Planet_battle_army (
-    planet_id NUMBER NOT NULL,
-    num_battle NUMBER NOT NULL,
-    PRIMARY KEY (planet_id, num_battle), 
-    FOREIGN KEY (planet_id) REFERENCES Planet_stats(planet_id),
-    light_hunter_built NUMBER,
-    light_hunter_destroyed NUMBER,
-    heavy_hunter_built NUMBER,
-    heavy_hunter_destroyed NUMBER,
-    battleship_built NUMBER,
-    battleship_destroyed NUMBER,
-    armored_ship_built NUMBER,
-    armored_ship_destroyed NUMBER
-);
-
--- Crear tabla Enemy_army
-CREATE TABLE Enemy_army (
-    planet_id NUMBER NOT NULL,
-    num_battle NUMBER NOT NULL,
-    PRIMARY KEY (planet_id, num_battle), 
-    FOREIGN KEY (planet_id) REFERENCES Planet_stats(planet_id),
-    light_hunter_threat NUMBER,
-    light_hunter_destroyed NUMBER,
-    heavy_hunter_threat NUMBER,
-    heavy_hunter_destroyed NUMBER,
-    battleship_threat NUMBER,
-    battleship_destroyed NUMBER,
-    armored_ship_threat NUMBER,
-    armored_ship_destroyed NUMBER
+CREATE TABLE battle(
+    planet_id NUMBER,
+    num_battles NUMBER PRIMARY KEY NOT NULL,
+    battle_stats CLOB,
+    battle_log CLOB,
+    FOREIGN KEY (planet_id) REFERENCES planet_stats(planet_id)
 );
 --tablas para las naves
 CREATE TABLE LIGHT_HUNTERS (
